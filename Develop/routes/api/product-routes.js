971d46342c0feaee744dcb61222adcdcd176f1a3
AlayC <bsinlee@icloud.com>
2022-12-02 04:15:41 -0600

    Delete a product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -109,8 +109,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedData) {
+      res.status(404).json({message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(deletedData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
