881e1bed259ccd42fe52aba56139ba97bab67587
AlayC <bsinlee@icloud.com>
2022-12-02 02:02:15 -0600

    add ProductTag column

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        //references to 'product' model under modelName property in Product.js
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        //references to 'tag'model under modelName property in Tag.js
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
